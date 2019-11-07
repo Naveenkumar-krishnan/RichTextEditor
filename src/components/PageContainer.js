6e1d906831af4f79f9cae02e6f8bef09a08f98a5
naveen kumar <naveen.august17@gmail.com>
2019-11-07 18:31:28 +0530

    first commit

---
+++ src/components/PageContainer.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import { Editor, EditorState, RichUtils } from 'draft-js';
+import BlockStyleToolbar, {
+  getBlockStyle
+} from "./blockStyles/BlockStyleToolbar";
+
+class PageContainer extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    }
+  }
+
+  
+  onChange = (editorState) => {
+    this.setState({
+      editorState 
+    }) 
+  }
+
+  handleKeyCommand = (command) => {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+
+  onUnderlineClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+  }
+
+  onBoldClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'))
+  }
+
+  onItalicClick = () => {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'))
+  }
+
+  toggleBlockType = blockType => {
+    this.onChange(RichUtils.toggleBlockType(this.state.editorState, blockType));
+  };
+
+
+  render() {
+    return(
+      <div className="editorContainer">
+        <div className="editorToolBar">
+          <BlockStyleToolbar
+              editorState={this.state.editorState}
+              onToggle={this.toggleBlockType}
+            />
+          <button className="styleButton"  onClick={this.onUnderlineClick}>U</button>
+          <button className="styleButton" onClick={this.onBoldClick}><b>B</b></button>
+          <button className="styleButton" onClick={this.onItalicClick}><em>I</em></button>  
+        </div>              
+        <div className="editors">
+          <Editor 
+            editorState={this.state.editorState}
+            handleKeyCommand={this.handleKeyCommand}
+            onChange= { this.onChange }
+            blockStyleFn={getBlockStyle}
+            />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PageContainer
